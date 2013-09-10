2acad82fcea160197f732ff3f65a833d941ea5dc
naathyn <nathan3k@gmail.com>
2013-09-10 01:01:30 -0500

    fix contact form

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,5 @@
 class Message < ActiveRecord::Base
-  validates_presence_of :department, :subject, :email, :body
+  validates_presence_of :subject, :email, :body
   validates_length_of :body, minimum: 15
   validates_length_of :subject, minimum: 5
-  DEPARTMENTS = [ ["Booking <booking@samraines.com>", "booking@samraines.com"],
-                  ["Press <press@samraines.com>", "press@samraines.com"],
-                  ["Sam <sam@samraines.com>", "sam@samraines.com"]
-                ]
 end
